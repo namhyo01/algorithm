123c31a4479a66617581c0e400e085a15da330d6
namhyo01@naver.com <namhyo01@naver.com>
2023-02-18 01:04:07 +0900

    11726

--- dp/11726.py
+++ dp/11726.py
@@ -4,8 +4,6 @@ n = int(input())
 dp = [0 for _ in range(1001)]
 dp[1] = 1
 dp[2] = 2
-dp[3] = 3
-dp[4] = 5
-for i in range(5,n+1):
+for i in range(3,n+1):
     dp[i] = (dp[i-1]+dp[i-2])%10007
 print(dp[n]%10007)
