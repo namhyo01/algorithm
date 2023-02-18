a42329453c44b08e86831d6df2f84d818159c511
namhyo01@naver.com <namhyo01@naver.com>
2023-02-18 16:44:11 +0900

    1904

--- dp/1904.py
+++ dp/1904.py
@@ -1,2 +1,12 @@
 import sys
 input = sys.stdin.readline
+n = int(input())
+mod = 15746
+dp = [0 for _ in range(1000001)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+dp[4] = 5
+for i in range(5,n+1):
+    dp[i] = (dp[i-1]+dp[i-2])%mod
+print(dp[n])
